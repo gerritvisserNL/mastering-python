978d2e88880aa803e78e3dfedbd95a453c9241dc
GERRIT VISSER <gervisz@live.nl>
2026-02-03 13:34:05 +0100

    Exercise 4: for loops and accumulation

---
+++ exercises/exertcise_04_loops.py
@@ -0,0 +1,16 @@
+for count in range(1, 11):
+    print(count)
+
+for count in range(1, 21):
+    if count % 2 == 0:
+        print(count)
+
+# for count in range(2, 21, 2):
+#     print(count)
+
+total = 0
+for count in range(1, 101):
+    total += count
+print("Total:", total)
+
+# print("Total:", sum(range(1, 101)))
