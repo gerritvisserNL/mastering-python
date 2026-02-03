4b017b173e3b0f08db2d7597ebe7bf4d25d40a72
GERRIT VISSER <gervisz@live.nl>
2026-02-03 12:49:18 +0100

    Exercise 1: variables and f-strings

---
+++ exercises/exercise_01_variables.py
@@ -0,0 +1,13 @@
+name = "Gerrit"
+age = 42
+city = "Amsterdam"
+
+print(name)
+print(age)
+print(city)
+
+print(f"Ik ben {name}, ik ben {age} jaar en ik woon in {city}")
+
+print(type(name))
+print(type(age))
+print(type(city))
